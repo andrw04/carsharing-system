c747e86680a19536facfa4dcbf0077d8de106fe9
andrw04 <sivyandrey2021@gmail.com>
2023-10-31 01:29:54 +0300

    corrections in docker-compose and init scripts

--- app/queries.py
+++ app/queries.py
@@ -1,1 +1,5 @@
-GET_USERS = """SELECT * FROM users;"""
+def get_users():
+    return "SELECT * FROM users;"
+
+def create_user(username, password, driving_license, first_name, last_name, email, phone):
+    return f"INSERT INTO users VALUES ('{username}', '{password}', '{driving_license}', '{first_name}', '{last_name}', '{email}', '{phone}', 0);"
